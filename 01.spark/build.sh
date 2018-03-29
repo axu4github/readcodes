fc6100b5b92163921c4a7bb9c3a4babbcb37a00e
axu4github <axu.home@gmail.com>
2018-03-29 23:06:11 +0800

    提交build程序

---
+++ 01.spark/build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+sub=$1
+
+function run_build() {
+    ./build/mvn -DskipTests -pl :$1 clean install
+}
+
+if [ -n "${SPARK_HOME}" ]; then
+    echo "SPARK_HOME: ${SPARK_HOME}"
+    cd ${SPARK_HOME}
+    export MAVEN_OPTS="-Xmx2g -XX:ReservedCodeCacheSize=512m"
+    if [ "${sub}" = "core" ]; then
+        artifactId="spark-core_2.11"
+    elif [ "${sub}" = "repl" ]; then
+        artifactId="spark-repl_2.11"
+    else
+        echo "NOT SET SUB."
+        exit
+    fi
+
+    run_build ${artifactId}
+
+    CMD="cp ${SPARK_HOME}/${sub}/target/${artifactId}-2.3.0.jar ${SPARK_HOME}/assembly/target/scala-2.11/jars/"
+    echo "${CMD}"
+    ${CMD}
+    
+    cd -
+else
+    echo "NOT SET SPARK_HOME."
+fi
